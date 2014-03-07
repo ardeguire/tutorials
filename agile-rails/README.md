d04c85aaee901e1c0772ce57ec19a44602675cf5
Adam De Guire <adam@ardeguire.com>
2014-03-07 16:29:11 -0500

    start agile-rails tutorial

---
+++ agile-rails/README.md
@@ -0,0 +1,1 @@
+Part 2: Building an Application of "Agile Web Development with Rails, 4th ed." by Ruby, et al.
---
+++ agile-rails/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b93766b3d6578d26b118f48b11bd87bc81faded7f67e53b9c16d039dbb6f19ffe25b28f3bf051fc49b44db68d3a25089bbb52fe56442d50b920dce3302491614'
